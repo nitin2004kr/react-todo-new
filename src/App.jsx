be3e2b4d6b90fd73c447835965b21356b24152f5
nitin2004kr <nitin112004kr@gmail.com>
2025-04-24 11:14:51 +0530

    tes
    tes

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import Sidebar from './components/sidebar/Sidebar'
 import Foreground from './components/foreground/Foreground'
 
 function App() {
- 
+
 
   return (
     <>
       <div className='w-full h-screen bg bg-zinc-100 flex' >
-          {/* --- sidebar navigation --- */}
-          <div className='h-full lg:w-1/5 md:w-1/4 w-37 py-5 px-2' >
-            <Sidebar/>
-          </div> 
+        {/* --- sidebar navigation --- */}
+        <div className='h-full lg:w-1/5 md:w-1/4 w-37 py-5 px-2' >
+          <Sidebar />
+        </div>
 
-          {/* --- foreground dashboard --- */}
-          <div className='h-full flex-grow py-5 px-2 ' >
-            <Foreground/> 
-          </div>
+        {/* --- foreground dashboard --- */}
+        <div className='h-full flex-grow py-5 px-2 ' >
+          <Foreground />
+        </div>
       </div>
     </>
   )
