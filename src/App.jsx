854a1cdf82eb51d79364911ed09d3cbe0e6fc2f7
nitin2004kr <nitin112004kr@gmail.com>
2025-06-29 11:21:12 +0530

    react-todo-app

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
 
   return (
     <div className='w-full h-screen bg bg-zinc-100 flex'>
+
       {isDashboard && (
         <div className='h-full lg:w-1/5 md:w-1/4 w-37 py-5 px-2'>
           <Sidebar />
@@ -20,6 +21,23 @@ function App() {
       )}
 
       <div className={`h-full ${isDashboard ? 'flex-grow py-5 px-2' : 'w-full'}`}>
+        {!isDashboard && (
+          <div className="w-ful border-b-2 border-[#3F7FAF]  bg-blue-100 flex justify-between items-center py-3 px-15 ">
+            <div className="w-[80px] ">
+              <img src="/assets/images/logo.png" />
+            </div>
+
+
+            <ul className="flex justify-center items-center gap-10 px-40">
+              <li className="text-2xl font-bold " style={{ color: '#3F7FAF' }}><a href='/'>Home</a></li>
+              <li className="text-2xl font-bold " style={{ color: '#3F7FAF' }}><a href='/dashboard/today-task'>All Tasks</a></li>
+              <li className="text-2xl font-bold " style={{ color: '#3F7FAF' }}>Pricing</li>
+              <li className="text-2xl font-bold " style={{ color: '#3F7FAF' }}><a href="/login">Login</a></li>
+            </ul>
+
+          </div>
+        )}
+
         <Routes>
           {/* Public or auth routes */}
           <Route path="/" element={<Home />} />
